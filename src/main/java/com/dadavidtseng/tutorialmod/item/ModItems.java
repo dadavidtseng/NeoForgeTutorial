540b5d2a9722b560c0894fe026b89c0973716088
Yu-Wei, Tseng <dadavidtseng@gmail.com>
2025-05-18 22:40:28 -0500

    update: custom food and fuel finished

--- src/main/java/com/dadavidtseng/tutorialmod/item/ModItems.java
+++ src/main/java/com/dadavidtseng/tutorialmod/item/ModItems.java
@@ -6,6 +6,7 @@
 package com.dadavidtseng.tutorialmod.item;
 
 import com.dadavidtseng.tutorialmod.TutorialMod;
+import com.dadavidtseng.tutorialmod.block.custom.FuelItem;
 import com.dadavidtseng.tutorialmod.item.custom.ChiselItem;
 import net.minecraft.world.item.Item;
 import net.neoforged.bus.api.IEventBus;
@@ -23,6 +24,14 @@ public class ModItems
     public static final DeferredItem<Item> RAW_BISMUTH = ITEMS.register("raw_bismuth", () -> new Item(new Item.Properties()));
     //----------------------------------------------------------------------------------------------------
     public static final DeferredItem<Item> CHISEL = ITEMS.register("chisel", () -> new ChiselItem(new Item.Properties().durability(32)));
+    //----------------------------------------------------------------------------------------------------
+    public static final DeferredItem<Item> RADISH = ITEMS.register("radish", () -> new Item(new Item.Properties().food(ModFoodProperties.RADISH)));
+    //----------------------------------------------------------------------------------------------------
+    public static final DeferredItem<Item> FROSTFIRE_ICE = ITEMS.register("frostfire_ice",
+            () -> new FuelItem(new Item.Properties(), 800));
+    //----------------------------------------------------------------------------------------------------
+    public static final DeferredItem<Item> STARLIGHT_ASHES = ITEMS.register("starlight_ashes",
+            () -> new Item(new Item.Properties()));
 
     //----------------------------------------------------------------------------------------------------
     public static void register(IEventBus eventBus)
--- src/main/java/com/dadavidtseng/tutorialmod/item/custom/ChiselItem.java
+++ src/main/java/com/dadavidtseng/tutorialmod/item/custom/ChiselItem.java
@@ -12,6 +12,8 @@ import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.UseAnim;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
@@ -59,5 +61,12 @@ public class ChiselItem extends Item
         }
 
         return InteractionResult.SUCCESS;
+    }
+
+    // Delete later
+    @Override
+    public UseAnim getUseAnimation(ItemStack stack)
+    {
+        return UseAnim.DRINK;
     }
 }
